024bbfcd7413e6e15e95c8d5d59a4c3e1920dff2
Hakidie <99023101+Hakidie@users.noreply.github.com>
2026-07-20 14:36:51 +0700

    upload

--- Lesson-01/practice/backend/index.js
+++ Lesson-01/practice/backend/index.js
@@ -4,18 +4,19 @@ import { getAllOrdersByCustomerId, getOrdersWithHighValue } from './routes/order
 import { getProducts } from './routes/products.js';
 
 const routes = [
-    { method: 'GET', pattern: /^\/$/, handler: (req, res) => res.end('Hello MindX') },
-    { method: 'GET', pattern: /^\/customers$/, handler: getAllCustomers },
-    { method: 'GET', pattern: /^\/customers\/([a-zA-Z0-9]+)$/, handler: getCustomerById },
-    { method: 'GET', pattern: /^\/customers\/([a-zA-Z0-9]+)\/orders$/, handler: getAllOrdersByCustomerId },
-    { method: 'GET', pattern: /^\/orders\/highvalue$/, handler: getOrdersWithHighValue },
-    { method: 'GET', pattern: /^\/products$/, handler: getProducts },
+    { method: 'GET', pattern: /^\/$/, handler: (req, res) => res.end('Hello MindX') }, // Landing page
+    { method: 'GET', pattern: /^\/customers$/, handler: getAllCustomers }, // 1. Get all customers
+    { method: 'GET', pattern: /^\/customers\/([a-zA-Z0-9]+)$/, handler: getCustomerById }, // 2. Get customer info by their ID
+    { method: 'GET', pattern: /^\/customers\/([a-zA-Z0-9]+)\/orders$/, handler: getAllOrdersByCustomerId }, // 3. Get order info of a customer by their ID
+    { method: 'GET', pattern: /^\/orders\/highvalue$/, handler: getOrdersWithHighValue }, // 4. Get orders with total price larger than 10 mil
+    { method: 'GET', pattern: /^\/products$/, handler: getProducts }, // 5. Get all products with price range
 ];
 
 const app = http.createServer((request, response) => {
-    const parsedUrl = new URL(request.url, `http://${request.headers.host}`);
-    const pathname = parsedUrl.pathname;
-
+    const parsedUrl = new URL(request.url, `http://${request.headers.host}`); // Put url in to URL obj for exercise 5
+    const pathname = parsedUrl.pathname; // Get path
+    
+    // Check route
     const route = routes.find(r =>
         r.method === request.method && r.pattern.test(pathname)
     );
@@ -26,9 +27,11 @@ const app = http.createServer((request, response) => {
         return;
     }
 
+    // Get params for exercise 2
     const match = pathname.match(route.pattern);
     const params = match.slice(1);
 
+    // Call function to handle excerises
     route.handler(request, response, parsedUrl.searchParams, ...params);
 });
 
