a87a2bc7fecef9e11b3e3404718bee9b3f723a68
Hakidie <99023101+Hakidie@users.noreply.github.com>
2026-08-11 22:05:39 +0700

    add Lesson 8

---
+++ Lesson-08/theory/backend/models/products.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+    _id: { type: String },
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+}, { _id: false });
+
+const ProductsModel = mongoose.model('products', productSchema);
+
+export default ProductsModel;
