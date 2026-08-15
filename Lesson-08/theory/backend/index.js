67f49358199ae47af9591fc81c765c4746274f8c
Hakidie <99023101+Hakidie@users.noreply.github.com>
2026-08-15 22:13:41 +0700

    Minor changes

--- Lesson-08/theory/backend/index.js
+++ Lesson-08/theory/backend/index.js
@@ -18,12 +18,28 @@ mongoose.connect(process.env.MONGO_URI);
 const app = express();
 app.use(express.json()); // For input body
 
+// Secret key, thường được lưu trong biến môi trường (env)
+const secretKey = 'mysecretkey';
 
+// Dữ liệu mẫu để mã hoá vào JWT
+const userData = {
+  id: "123",
+  username: 'john_doe',
+  role: 'user',
+};
 
+// Tạo JWT
+const token = jwt.sign(userData, secretKey, { expiresIn: '1h' });
 
-
-
-
+// Xác thực JWT
+jwt.verify(token, secretKey, (err, decoded) => {
+  if (err) {
+    console.error('JWT verification failed:', err.message);
+  } else {
+    console.log('Decoded JWT:');
+    console.log(decoded);
+  }
+});
 
 app.use('/', customerRoutes);
 app.use('/', orderRoutes);
